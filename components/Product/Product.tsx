9ce036453b9272ed9d7be6c81097c5af0360c6d8
Anna <masav1997@gmail.com>
2021-10-09 14:07:22 +0300

    adding availability and metrics

--- components/Product/Product.tsx
+++ components/Product/Product.tsx
@@ -28,6 +28,7 @@ export const Product = motion(forwardRef(({ product, className, ...props }: Prod
 			behavior: 'smooth',
 			block: 'start'
 		});
+		reviewRef.current?.focus();
 	};
 
 	return (
@@ -43,16 +44,23 @@ export const Product = motion(forwardRef(({ product, className, ...props }: Prod
 				</div>
 				<div className={styles.title}>{product.title}</div>
 				<div className={styles.price}>
-					{priceRu(product.price)}
-					{product.oldPrice && <Tag className={styles.oldPrice} color="green">{priceRu(product.price - product.oldPrice)}</Tag>}
+					<span><span className="visualyHidden">цена</span>{priceRu(product.price)}</span>
+					{product.oldPrice && <Tag className={styles.oldPrice} color="green">
+						<span className="visualyHidden">скидка</span>
+						{priceRu(product.price - product.oldPrice)}
+					</Tag>}
 				</div>
 				<div className={styles.credit}>
+					<span className="visualyHidden">кредит</span>
 					{priceRu(product.credit)}/<span className={styles.month}>мес</span>
 				</div>
-				<div className={styles.rating}><Rating rating={product.reviewAvg ?? product.initialRating} /></div>
+				<div className={styles.rating}>
+					<span className="visualyHidden">{'рейтинг' + (product.reviewAvg ?? product.initialRating)}</span>
+					<Rating rating={product.reviewAvg ?? product.initialRating} />
+				</div>
 				<div className={styles.tags}>{product.categories.map(c => <Tag key={c} className={styles.category} color='ghost'>{c}</Tag>)}</div>
-				<div className={styles.priceTitle}>цена</div>
-				<div className={styles.creditTitle}>кредит</div>
+				<div className={styles.priceTitle} aria-hidden={true}>цена</div>
+				<div className={styles.creditTitle} aria-hidden={true}>кредит</div>
 				<div className={styles.rateTitle}><a href="#ref" onClick={scrollToReview}>{product.reviewCount} {declOfNum(product.reviewCount, ['отзыв', 'отзыва', 'отзывов'])}</a></div>
 				<Divider className={styles.hr} />
 				<div className={styles.description}>{product.description}</div>
@@ -83,18 +91,19 @@ export const Product = motion(forwardRef(({ product, className, ...props }: Prod
 						arrow={isReviewOpened ? 'down' : 'right'}
 						className={styles.reviewButton}
 						onClick={() => setIsReviewOpened(!isReviewOpened)}
+						aria-expanded={isReviewOpened}
 					>Читать отзывы</Button>
 				</div>
 			</Card>
 			<motion.div animate={isReviewOpened ? 'visible' : 'hidden'} variants={variants} initial='hidden'>
-				<Card color='blue' className={styles.reviews} ref={reviewRef}>
+				<Card color='blue' className={styles.reviews} ref={reviewRef} tabIndex={isReviewOpened ? 0 : -1}>
 					{product.reviews.map(r => (
 						<div key={r._id}>
 							<Review review={r} />
 							<Divider />
 						</div>
 					))}
-					<ReviewForm productId={product._id} />
+					<ReviewForm productId={product._id} isOpened={isReviewOpened} />
 				</Card>
 			</motion.div>
 		</div>
--- page-components/TopPageComponent/TopPageComponent.tsx
+++ page-components/TopPageComponent/TopPageComponent.tsx
@@ -6,9 +6,11 @@ import { TopLevelCategory } from '../../interfaces/page.interface';
 import { SortEnum } from '../../components/Sort/Sort.props';
 import { useEffect, useReducer } from 'react';
 import { sortReducer } from './sort.reducer';
+import { useReducedMotion } from 'framer-motion';
 
 export const TopPageComponent = ({ page, products, firstCategory }: TopPageComponentProps): JSX.Element => {
 	const [{ products: sortedProducts, sort }, dispathSort] = useReducer(sortReducer, { products, sort: SortEnum.Rating });
+	const shouldReduceMotion = useReducedMotion();
 
 	const setSort = (sort: SortEnum) => {
 		dispathSort({ type: sort });
@@ -22,11 +24,11 @@ export const TopPageComponent = ({ page, products, firstCategory }: TopPageCompo
 		<div className={styles.wrapper}>
 			<div className={styles.title}>
 				<Htag tag='h1'>{page.title}</Htag>
-				{products && <Tag color='grey' size='m'>{products.length}</Tag>}
+				{products && <Tag color='grey' size='m' aria-label={products.length + 'элементов'}>{products.length}</Tag>}
 				<Sort sort={sort} setSort={setSort} />
 			</div>
-			<div>
-				{sortedProducts && sortedProducts.map(p => (<Product layout key={p._id} product={p} />))}
+			<div role='list'>
+				{sortedProducts && sortedProducts.map(p => (<Product role='listitem' layout={shouldReduceMotion ? false : true} key={p._id} product={p} />))}
 			</div>
 			<div className={styles.hhTitle}>
 				<Htag tag='h2'>Вакансии - {page.category}</Htag>
